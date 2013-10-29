76912ab6d3ad54f4df0ba40c14a1d0ffedd91bd8
Tim G. Thomas <tim@timgthomas.com>
2013-10-29 11:21:33 -0500

    wire up sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,24 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          sourcemap: true,
+          style: 'expanded'
+        },
+        files: {
+          'dist/style.css': 'css/style.scss'
+        }
+      }
+    }
+
   });
 
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['sass']);
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
 };
